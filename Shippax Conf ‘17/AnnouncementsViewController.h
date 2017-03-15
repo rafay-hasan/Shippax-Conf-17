7152c9b0d463b5a87b40d195f5cee676db09beb7
HasanRafay <rafay@nascenia.com>
2017-03-15 19:05:15 +0600

    Added - Tab bar, app icon, splash screen

---
+++ Shippax Conf ‘17/AnnouncementsViewController.h
@@ -0,0 +1,13 @@
+//
+//  AnnouncementsViewController.h
+//  Shippax Conf ‘17
+//
+//  Created by Rafay Hasan on 3/15/17.
+//  Copyright © 2017 Rafay Hasan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AnnouncementsViewController : UIViewController
+
+@end
